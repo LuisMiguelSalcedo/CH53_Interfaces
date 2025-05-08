92d0f727aa149d95444b5f7499a2666802db31f1
Luismi <luismisalga@gmail.com>
2025-05-08 10:17:42 -0600

    Se agregan los archivos main.java y triangulo.java para iniciar este ejercicio

---
+++ Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Triangulo t1 = new Triangulo("Bermudas", 20, 10, 15);
+		Triangulo t = new Triangulo("Triangulo T", 200, 100, 150);
+		Main.imprimirCalculos(t1);
+		Main.imprimirCalculos(t);
+	}//main
+	
+	public static void imprimirCalculos(Triangulo t){
+		System.out.println(t);
+		System.out.println("+-----------------------------------+");
+		System.out.println("| El area de [ " + t.getNombre()
+				+ "]");
+		System.out.println("| es " + t.calcularArea());
+		System.out.println("| El perimetro de [ " +t.getNombre()
+				+ "]");
+		System.out.println("| es " + t.calcularPerimetro());
+		System.out.println("+-----------------------------------+");
+	}
+	
+}//class Main
